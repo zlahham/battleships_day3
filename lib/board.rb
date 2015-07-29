ac3c08152cf510f97386439c4e9e7fd224112c96
lewis <ljones140@gmail.com>
2015-07-29 18:45:27 +0100

    refactoring

--- lib/board.rb
+++ lib/board.rb
@@ -6,16 +6,21 @@ class Board
 
   attr_reader :ships, :grid
 
-  def initialize (options = {container: Grid})
+  def initialize (options = { container: Grid })
     @container = options[:container]
     @ships = []
     @grid = @container.create
   end
 
   def place(ship, coordinate, grid = @grid)
-    @ships << ship
+    add_ship ship
     @grid.insert(ship, coordinate)
     ship.confirm_place
   end
+
+  def add_ship ship
+    @ships << ship
+  end
+
 end
 
